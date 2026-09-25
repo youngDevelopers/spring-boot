daae4ecfe422687083f33ce0ffec6a2176867d51
Emmanuel Damba <dambaemmanuel5@gmail.com>
2026-09-25 19:18:08 +0200

    Get Request -Hello World

--- src/main/java/com/codewithmaverick/springboot/Application.java
+++ src/main/java/com/codewithmaverick/springboot/Application.java
@@ -2,12 +2,19 @@ package com.codewithmaverick.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World, Welcome to Spring Boot!!!";
+    }
 }
